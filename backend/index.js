367c7578bac075378fa0d2e6db2a047fb62b4430
Ahsan Ali <ahsan326ali@gmail.com>
2025-07-12 18:04:54 +0500

    fix: cors

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.json()); 
 app.use(cookieParser());
 const corsOption={
-    origin:['http://localhost:3000','http://localhost:5173'],
+    origin:['whatsapp-clone-ndo61r27w-ahsans-codes-projects.vercel.app','http://localhost:5173',"https://whatsapp-clone-black-theta.vercel.app/"],
     credentials:true
 };
 app.use(cors(corsOption)); 
